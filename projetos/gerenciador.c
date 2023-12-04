f0cb68aa2c1d1bba98e0fd1be9f4d9e236e1290c
Nael <willowitzy@gmail.com>
2023-12-04 07:50:12 -0300

    (projeto) experimentando verificações de segurança scanf(), fgets() e limpeza de buffer ainda me confunde muito...

--- projetos/gerenciador.c
+++ projetos/gerenciador.c
@@ -40,6 +40,10 @@ typedef struct {
     int progresso;   // Progresso da tarefa em porcentagem (inicialmente 0%)
 } Tarefa;
 
+/* 
+Comando de Execução: 
+    gcc projetos/gerenciador.c -o build/gerenciador.out  && ./build/gerenciador.out
+*/
 int main() {
     setlocale(LC_ALL,"Portuguese_Brazil");
     
@@ -67,40 +71,71 @@ int main() {
     while(opcao != SAIR) { 
         exibirMenu();
 
+        printf("Digite a opcao desejada: ");
+
         // Usuário escolhe uma opção
-        scanf("%d", &opcao);
-        limpaBuffer();
+        if (scanf("%d", &opcao) != 1) {
+            printf("\nOpção inválida. Insira um número válido.\n");
+            continue;
+        }
+
+        // Verificar se a opção está dentro do intervalo válido (0-8)
+        if (opcao < SAIR || opcao > VISUALIZAR_ANDAMENTO_PROJETO) {
+            printf("\nOpção inválida. Insira um número correspondente a uma opção do menu.\n");
+            continue;
+        }
 
         // Limpa a tela quando uma opção é selecionada
         limpaTela(); 
         
         // FUNÇÃO 1: Editar o nome do projeto
         if (opcao == EDITAR_NOME_PROJETO) {
+            limpaBuffer();
             printf("Nome Atual do Projeto: %s\n\n", nomeProjeto);
             printf("Inserir novo nome? 1-Sim 2-Nao\n");
             scanf("%d", &sOuN);
-            limpaBuffer();
             limpaTela();
             
-            // Subtela "Sim" / "Não"
-            if(sOuN == 1) {
+            // "Sim"
+            if(sOuN == 1) { 
                 printf("Digite o novo nome do Projeto:\n");
-
+                
+                limpaBuffer();
                 // Usuário escolhe um nome pro projeto
                 fgets(novoNome, sizeof(novoNome), stdin);
-                novoNome[strcspn(novoNome, "\n")] = '\0';  // Remove o '\n' adicionado na hora do "Enter"
+                // Checando se o nome inserido ultrapassa o valor máximo de caracteres
+                if (strlen(novoNome) == 0 || strlen(novoNome) >= MAXIMO - 1) {
+                    printf("Nome do projeto inválido.\n");
 
-                // Adiciona o novo nome do projeto
-                strcpy(nomeProjeto, novoNome);
+                    // Limpar o buffer para evitar loop infinito
+                    limpaBuffer();
+                    limpaTela();
+                    continue; 
+                }
+                else { // O nome é válido
+                    novoNome[strcspn(novoNome, "\n")] = '\0';  // Remove o '\n' adicionado na hora do "Enter"
 
+                    // Adiciona o novo nome do projeto
+                    strcpy(nomeProjeto, novoNome);
+                    limpaBuffer();
+                    limpaTela();
+                }
+            // "Não"
+            } else if(sOuN == 0) {
+                limpaBuffer();
                 limpaTela();
-            } else if(sOuN == 0){
+            }
+            // Inválido
+            else {
+                printf("Opção inválida.\n");
+                limpaBuffer();
                 limpaTela();
             }
         }
         
         // FUNÇÃO 2: Adicionar integrantes
         if(opcao == ADICIONAR_INTEGRANTE) {
+            limpaBuffer();
             printf("Quantos Integrantes Deseja Adicionar? ");
             scanf("%d", &novosIntegrantes);
 
@@ -118,7 +153,7 @@ int main() {
 
             //printf("Quantidade de integrantes: %d\n", qtdIntegrantes); // DEBUG
 
-            for(integrante = qtdIntegrantesAnterior; integrante < qtdIntegrantes; integrante++){
+            for(integrante = qtdIntegrantesAnterior; integrante < qtdIntegrantes; integrante++) {
                 printf("Digite o nome do integrante %d:\n", integrante);
                 fgets(pessoa[integrante].nome, sizeof(pessoa[integrante].nome), stdin);
                 pessoa[integrante].nome[strcspn(pessoa[integrante].nome, "\n")] = '\0';  // Remove o '\n' adicionado na hora do "Enter"
@@ -136,10 +171,11 @@ int main() {
         int idExclusao; // Número do cara que vai ser excluido
         char valorNulo[5] = "Null";
         sOuN = 1;
-        if(opcao == REMOVER_INTEGRANTE){
-            while(sOuN == 1){ 
+        if(opcao == REMOVER_INTEGRANTE) {
+            while(sOuN == 1) { 
                 // Validar a entrada para idExclusao verificando se scanf retorna 1 (indica que um inteiro foi lido com sucesso)
                 printf("Digite o numero do integrante a ser removido: ");
+
                 if (scanf("%d", &idExclusao) != 1) {
                     printf("Entrada inválida. Digite um número válido.\n");
                     limpaBuffer();
@@ -159,15 +195,19 @@ int main() {
                         strcpy(pessoa[integrante].funcao, valorNulo);
                     }
                 }
-                
+
+                limpaBuffer();
                 limpaTela();
+
                 printf("Integrante Removido. Deseja remover mais algum? 1-Sim 2-Nao\n");
+
                 if (scanf("%d", &sOuN) != 1) {
                     // Ao pedir ao usuário para escolher, validar novamente a entrada para garantir que seja ou 1 ou 2
                     printf("Entrada inválida. Digite 1 para continuar ou 2 para sair.\n");
                     limpaBuffer();
                     continue;
                 }
+                limpaBuffer();
                 limpaTela();
             }
         }
@@ -186,15 +226,16 @@ int main() {
 
             // Voltar para o menu principal
             int voltar = 1;
+            limpaBuffer();
             printf("\nDigite 0 para voltar: ");
             scanf("%d", &voltar);
-            limpaBuffer();
             limpaTela();
         }
 
         // FUNÇÃO 5: Adicionar tarefa
         if (opcao == ADICIONAR_TAREFA) {
             while(1) {
+                limpaBuffer();
                 if (qtdTarefas < MAXIMO) {
                     // Tarefa
                     printf("Digite o nome da tarefa: ");
@@ -207,6 +248,7 @@ int main() {
                         printf("Digite o nome do responsavel pela tarefa: ");
                         fgets(tarefas[qtdTarefas].responsavel, sizeof(tarefas[qtdTarefas].responsavel), stdin);
                         tarefas[qtdTarefas].responsavel[strcspn(tarefas[qtdTarefas].responsavel, "\n")] = '\0';  // Remove o '\n' adicionado na hora do "Enter"
+                        // Nesse tipo de situação acima não é necessário usar limpaBuffer()
 
                         int responsavelID = -1;
                         for (int i = 0; i < qtdIntegrantes; i++) {
@@ -224,14 +266,15 @@ int main() {
                             // Perguntar se o usuário quer adicionar outra tarefa
                             printf("Deseja tentar novamente? 1-Sim 2-Não\n");
                             scanf("%d", &sOuN);
-                            limpaBuffer();
 
                             if (sOuN == 1) {
                                 // Volta pro loop de adicionar integrante
+                                limpaBuffer();
                                 limpaTela();
                                 continue;
                             }
                             else {
+                                limpaBuffer();
                                 limpaTela();
                                 break;
                             }
@@ -250,7 +293,6 @@ int main() {
                     // Perguntar se o usuário quer adicionar outra tarefa
                     printf("Deseja adicionar outra tarefa? 1-Sim 2-Não\n");
                     scanf("%d", &sOuN);
-                    limpaBuffer();
 
                     if (sOuN != 1) {
                         // Sai do loop de adicionar tarefas
@@ -266,10 +308,10 @@ int main() {
 
     // FUNÇÃO 6: Remover tarefa
     if (opcao == REMOVER_TAREFA) {
+        limpaBuffer();
         int numeroTarefa;
         printf("Digite o número da tarefa que deseja remover: ");
         scanf("%d", &numeroTarefa);
-        limpaBuffer();
 
         // Verificar se o número da tarefa é válido
         if (numeroTarefa >= 0 && numeroTarefa < qtdTarefas) {
@@ -290,10 +332,10 @@ int main() {
 
     // FUNÇÃO 7: Editar tarefa
     if (opcao == EDITAR_TAREFA) {
+        limpaBuffer();
         int numeroTarefa;
         printf("Digite o número da tarefa que deseja editar: ");
         scanf("%d", &numeroTarefa);
-        limpaBuffer();
 
         // Verificar se o número da tarefa é válido
         if (numeroTarefa >= 0 && numeroTarefa < qtdTarefas) {
@@ -305,9 +347,9 @@ int main() {
             fgets(tarefas[numeroTarefa].responsavel, sizeof(tarefas[numeroTarefa].responsavel), stdin);
             tarefas[numeroTarefa].responsavel[strcspn(tarefas[numeroTarefa].responsavel, "\n")] = '\0';
 
+            limpaBuffer();
             printf("Digite o progresso atual [%%]: ");
             scanf("%d", &tarefas[numeroTarefa].progresso);
-            limpaBuffer();
 
             // Verificar se o responsável é um integrante válido
             int responsavelID = -1;
@@ -343,9 +385,9 @@ int main() {
 
             // Voltar para o menu principal
             int voltar = 1;
+            limpaBuffer();
             printf("\nDigite 0 para voltar: ");
             scanf("%d", &voltar);
-            limpaBuffer();
             limpaTela();
         }
         // FIM DO LOOP PRINCIPAL
@@ -396,3 +438,4 @@ void exibirMenu() {
         printf("#########################################################################################################\n");
 }
 
+// (Testar depois refazer o loop do menu com switch case)
